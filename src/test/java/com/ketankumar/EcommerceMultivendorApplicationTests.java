26e257d31b4e637b3bbaafa833b722448b88ed06
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-12-29 01:41:14 +0000

    first commit

---
+++ src/test/java/com/ketankumar/EcommerceMultivendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketankumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMultivendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
